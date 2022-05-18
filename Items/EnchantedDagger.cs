b7b9d9bf2457526337a34234e68e816c919712d7
wdfeer <wdf-is-git@tutanota.de>
2022-05-18 08:56:43 +0200

    Add the Enchanted Dagger magic weapon

---
+++ Items/EnchantedDagger.cs
@@ -0,0 +1,45 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using wdfeerCrazyMod.Projectiles;
+
+namespace wdfeerCrazyMod.Items
+{
+	public class EnchantedDagger : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Throws a magic enchanted dagger that homes in on your foes, kinda");
+		}
+		public override void SetDefaults()
+		{
+			Item.damage = 32;
+			Item.crit = 12;
+			Item.DamageType = DamageClass.Magic;
+			Item.mana = 6;
+			Item.noMelee = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.scale = 0;
+			Item.useTime = 10;
+			Item.useAnimation = 10;
+			Item.knockBack = 4;
+			Item.value = Item.buyPrice(silver: 50);
+			Item.rare = 5;
+			Item.UseSound = SoundID.Item19;
+			Item.autoReuse = true;
+			Item.shootSpeed = 16;
+			Item.shoot = ModContent.ProjectileType<Projectiles.EnchantedDagger>();
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.MagicDagger);
+			recipe.AddIngredient(ItemID.SoulofLight, 6);
+			recipe.AddTile(TileID.Anvils);
+			recipe.Register();
+		}
+    }
+}
---
+++ Projectiles/EnchantedDagger.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace wdfeerCrazyMod.Projectiles
+{
+    internal class EnchantedDagger : ModProjectile
+    {
+        public override string Texture => "wdfeerCrazyMod/Items/EnchantedDagger";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Enchanted Dagger");
+        }
+        int baseTimeLeft = 0;
+        public override void SetDefaults()
+        {
+            baseTimeLeft = 100;
+            Projectile.CloneDefaults(ProjectileID.MagicDagger);
+            Projectile.timeLeft = baseTimeLeft;
+            Projectile.penetrate = 4;
+        }
+        public override void AI()
+        {
+            Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + MathHelper.PiOver2;
+            if (Projectile.timeLeft == baseTimeLeft - 15)
+            {
+                Vector2 target = FindTarget();
+                if (target != Vector2.Zero)
+                    Projectile.velocity = Projectile.velocity.Length() * (target - Projectile.Center).SafeNormalize(Vector2.Zero);
+            }
+            if (Projectile.timeLeft < 20)
+            {
+                Projectile.alpha = 255 - (255 * Projectile.timeLeft / 20);
+            }
+        }
+        private Vector2 FindTarget()
+        {
+            var potentialTarget = Main.npc
+                .Where(npc => npc.active && !npc.friendly && npc.CanBeChasedBy())
+                .MinBy(npc => (npc.Center - Projectile.Center).Length());
+            if (potentialTarget != null && potentialTarget.Center.Distance(Projectile.Center) < 600)
+                return potentialTarget.Center;
+            return Vector2.Zero;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.timeLeft = baseTimeLeft;
+        }
+    }
+}
