6b5b45a868fbc3762399728e52e589e4b075b457
wdfeer <wdf-is-git@tutanota.de>
2022-05-18 14:58:42 +0200

    Add the Autobow ranged weapon

---
+++ Projectiles/AutobowProjectile.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace wdfeerCrazyMod.Projectiles
+{
+    internal class AutobowProjectile : ModProjectile
+    {
+        public override string Texture => "wdfeerCrazyMod/Items/Autobow";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Autobow");
+        }
+        int baseTimeLeft = 0;
+        public override void SetDefaults()
+        {
+            baseTimeLeft = 480;
+            Projectile.width = 18;
+            Projectile.height = 36;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.penetrate = -1;
+        }
+        public override void AI()
+        {
+            if (Projectile.velocity.Y < 16)
+                Projectile.velocity.Y += 0.12f;
+            if (Projectile.timeLeft % 10 == 0)
+            {
+                Vector2 target = GetTarget();
+                if (target != Vector2.Zero)
+                {
+                    Fire(target);
+
+                    SoundEngine.PlaySound(SoundID.Item5);
+
+                    Vector2 vectorToTarget = target - Projectile.Center;
+                    Projectile.rotation = (float)Math.Atan2(vectorToTarget.Y, vectorToTarget.X);
+                }
+            }
+            if (Projectile.timeLeft < 20)
+            {
+                Projectile.alpha = 255 - (255 * Projectile.timeLeft / 20);
+            }
+        }
+        private void Fire(Vector2 target)
+        {
+            Vector2 launchVelocity = (target - Projectile.Center).SafeNormalize(Vector2.Zero) * 16;
+            Projectile arrow = Projectile.NewProjectileDirect(Projectile.InheritSource(Projectile),
+                                                            Projectile.Center,
+                                                            launchVelocity,
+                                                            ProjectileID.WoodenArrowFriendly,
+                                                            Projectile.damage / 2,
+                                                            Projectile.knockBack / 2,
+                                                            Projectile.owner);
+            arrow.usesLocalNPCImmunity = true;
+            arrow.localNPCHitCooldown = -1;
+        }
+        private Vector2 GetTarget()
+        {
+            var potentialTarget = Main.npc
+                .Where(npc => npc.active && !npc.friendly && npc.CanBeChasedBy() && LineOfSight(npc.position, npc.width, npc.height))
+                .MinBy(npc => (npc.Center - Projectile.Center).Length());
+            if (potentialTarget != null && potentialTarget.Center.Distance(Projectile.Center) < 900)
+                return potentialTarget.Center;
+            return Vector2.Zero;
+        }
+        private bool LineOfSight(Vector2 position, int width, int height)
+            => Collision.CanHitLine(Projectile.position, Projectile.width, Projectile.height, position, width, height);
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(Projectile.width, Projectile.width),
+                                    DustID.Titanium);
+            }
+        }
+    }
+}
