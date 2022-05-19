d88e1ddaed348d81cc1b529e8ea2e9846c9bf7c7
wdfeer <wdf-is-git@tutanota.de>
2022-05-19 07:55:22 +0200

    Add the PossessedUzi minion

---
+++ Items/PossessedUzi.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.GameContent.Creative;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace wdfeerCrazyMod.Items
+{
+    internal class PossessedUzi : ModItem
+    {
+        public override string Texture => "Terraria/Images/Item_" + ItemID.Uzi;
+        public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Summons a possessed uzi to fight for you");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+			ItemID.Sets.GamepadWholeScreenUseRange[Item.type] = true; // This lets the player target anywhere on the whole screen while using a controller
+			ItemID.Sets.LockOnIgnoresCollision[Item.type] = true;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 25;
+			Item.knockBack = 1f;
+			Item.mana = 14; // mana cost
+			Item.useTime = 30;
+			Item.useAnimation = 30;
+			Item.scale = 0;
+			Item.useStyle = ItemUseStyleID.Swing; // how the player's arm moves when using the item
+			Item.value = Item.sellPrice(gold: 6);
+			Item.rare = 7;
+			Item.UseSound = SoundID.Item44; // What sound should play when using the item
+
+			// These below are needed for a minion weapon
+			Item.noMelee = true; // this item doesn't do any melee damage
+			Item.DamageType = DamageClass.Summon; // Makes the damage register as summon. If your item does not have any damage type, it becomes true damage (which means that damage scalars will not affect it). Be sure to have a damage type
+			Item.buffType = ModContent.BuffType<Buffs.PossessedUziBuff>();
+			// No buffTime because otherwise the item tooltip would say something like "1 minute duration"
+			Item.shoot = ModContent.ProjectileType<Projectiles.PossessedUziProjectile>(); // This item creates the minion projectile
+		}
+
+		public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type, ref int damage, ref float knockback)
+		{
+			position = player.Center + new Vector2(0, -48);
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			player.AddBuff(Item.buffType, 2);
+
+			int projectileID = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            Main.projectile[projectileID].originalDamage = Item.damage;
+
+			return false;
+		}
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Uzi)
+				.AddIngredient(ItemID.SoulofNight, 16)
+				.AddTile(TileID.MythrilAnvil)
+				.Register();
+		}
+	}
+}
