172f3c520e2686a6d61341ba93b0b051b19d6a13
wdfeer <wdf-is-git@tutanota.de>
2022-05-24 22:23:34 +0200

    Add the Starbreak summon weapon

---
+++ Items/Starbreak.cs
@@ -0,0 +1,61 @@
+using Microsoft.Xna.Framework;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using wdfeerCrazyMod.Projectiles;
+
+namespace wdfeerCrazyMod.Items
+{
+	public class Starbreak : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Starbreak");
+			Tooltip.SetDefault("Holding this item materialises an interstellar spear, attacking your foes while they are nearby");
+		}
+		public override void SetDefaults()
+		{
+			Item.damage = 101;
+			Item.DamageType = DamageClass.Summon;
+			Item.knockBack = 8;
+			Item.value = Item.buyPrice(gold:22);
+			Item.rare = 9;
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.FragmentStardust, 12);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.Register();
+		}
+		Projectile projectile;
+        public override void HoldItem(Player player)
+        {
+			if (Main.myPlayer != player.whoAmI)
+				return;
+			NPC[] nearbyEnemies = Main.npc.Where(npc => npc is { active: true, friendly: false } && npc.CanBeChasedBy() && (player.Center - npc.Center).Length() < 900).ToArray();
+			if (nearbyEnemies.Length == 0)
+				return;
+			if (projectile == null || !projectile.active || projectile.type != ModContent.ProjectileType<Projectiles.StarbreakProjectile>() || projectile.owner != player.whoAmI)
+            {
+				NPC target = nearbyEnemies.MinBy(npc => (player.Center - npc.Center).Length());
+				Vector2 spawnPosition = target.Center + Main.rand.NextVector2CircularEdge(target.width + 240, target.height + 240);
+                Vector2 velocity = Vector2.Normalize(target.Center - spawnPosition) * 16;
+
+                int projectileID = Projectile.NewProjectile(Item.GetSource_ItemUse(Item),
+                                          spawnPosition,
+                                          velocity,
+                                          ModContent.ProjectileType<Projectiles.StarbreakProjectile>(),
+                                          player.GetWeaponDamage(Item),
+                                          Item.knockBack,
+										  player.whoAmI);
+				projectile = Main.projectile[projectileID];
+				projectile.CritChance = player.GetWeaponCrit(Item);
+				projectile.netUpdate = true;
+            }
+        }
+    }
+}
---
+++ Projectiles/StarbreakProjectile.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace wdfeerCrazyMod.Projectiles
+{
+    internal class StarbreakProjectile : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Starbreak");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.friendly = true;
+            Projectile.timeLeft = 60;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.extraUpdates = 3;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+        }
+        public override void AI()
+        {
+            Projectile.rotation = (float)Math.Atan2(Projectile.velocity.Y, Projectile.velocity.X) + MathHelper.PiOver2;
+        }
+        public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+        {
+            Projectile.timeLeft = 1;
+        }
+        public override void Kill(int timeLeft)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Dust d = Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(60,60), DustID.Firework_Blue);
+                d.noGravity = true;
+            }
+        }
+    }
+}
