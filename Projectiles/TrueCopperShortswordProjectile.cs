38d3bf5cfef2a55c6b0369bb0dc2aea6473bea7e
wdfeer <wdf-is-git@tutanota.de>
2022-05-25 08:11:19 +0200

    Add the TrueCopperShortsword melee

---
+++ Projectiles/TrueCopperShortswordProjectile.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace wdfeerCrazyMod.Projectiles
+{
+    internal class TrueCopperShortswordProjectile : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.CopperShortswordStab;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Copper Shortsword");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Melee;
+            Projectile.friendly = true;
+            Projectile.hostile = false;
+            Projectile.tileCollide = false;
+            Projectile.timeLeft = 15;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 5;
+        }
+        public override void AI()
+        {
+            if (Projectile.velocity.Length() > 1)
+                Projectile.rotation = Projectile.velocity.ToRotation() + MathHelper.PiOver4;
+            if (Projectile.timeLeft < 15)
+            {
+                Projectile.alpha = 255 - (255 * Projectile.timeLeft / 15);
+            }
+        }
+    }
+}
---
+++ Weapons/TrueCopperShortsword.cs
@@ -0,0 +1,67 @@
+using Microsoft.Xna.Framework;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+using wdfeerCrazyMod.Projectiles;
+
+namespace wdfeerCrazyMod.Weapons
+{
+	public class TrueCopperShortsword : ModItem
+	{
+		public override void SetDefaults()
+		{
+			Item.damage = 90;
+			Item.DamageType = DamageClass.Melee;
+			Item.noMelee = true;
+			Item.useStyle = ItemUseStyleID.Shoot;
+			Item.scale = 0;
+			Item.width = 56;
+			Item.height = 60;
+			Item.useTime = 13;
+			Item.useAnimation = 13;
+			Item.knockBack = 4;
+			Item.value = Item.buyPrice(gold: 12);
+			Item.rare = 9;
+			Item.autoReuse = true;
+			Item.shootSpeed = 12;
+			Item.shoot = ModContent.ProjectileType<Projectiles.TrueCopperShortswordProjectile>();
+		}
+
+		public override void AddRecipes()
+		{
+			Recipe recipe = CreateRecipe();
+			recipe.AddIngredient(ItemID.EmpressBlade);
+			recipe.AddIngredient(ItemID.CopperShortsword);
+			recipe.AddTile(TileID.LunarCraftingStation);
+			recipe.Register();
+		}
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+			NPC target = FindTarget(player);
+			if (target == null)
+				return true;
+			Vector2 targetCenter = target.Center;
+			Vector2 currentDirrection = Vector2.Normalize(velocity);
+            for (int i = 0;i < 25 && position.Distance(targetCenter) > 24; i++) {
+				position += currentDirrection * 48;
+				Vector2 toTarget = targetCenter - position;
+				currentDirrection = Vector2.Normalize(currentDirrection + toTarget * i / 3000);
+
+				int projectileID = Projectile.NewProjectile(source, position, Vector2.Zero, type, damage, knockback, player.whoAmI);
+				Projectile projectile = Main.projectile[projectileID];
+				projectile.CritChance = player.GetWeaponCrit(Item);
+				projectile.rotation = currentDirrection.ToRotation() + MathHelper.PiOver4;
+			}
+			return false;
+		}
+		private NPC FindTarget(Player player)
+        {
+			NPC target = Main.npc.Where(npc => npc.CanBeChasedBy() && !npc.friendly).MinBy(npc => (npc.Center - player.Center).Length());
+			if (target != null && target.Center.Distance(player.Center) < 840)
+				return target;
+			return null;
+        }
+    }
+}
