91f637f7649512321795798f522907ad2bc55e7a
wdfeer <wdf-is-git@tutanota.de>
2022-09-02 08:06:24 +0200

    Make Scorcher lootable

--- NPCLoot.cs
+++ NPCLoot.cs
@@ -15,6 +15,8 @@ internal class NPCLoot : GlobalNPC
                 return ItemDropRule.Common(ModContent.ItemType<EnchantedUmbrella>(), 14);
             case NPCID.MossHornet or NPCID.BigMossHornet or NPCID.GiantMossHornet or NPCID.TinyMossHornet or NPCID.LittleMossHornet:
                 return ItemDropRule.Common(ModContent.ItemType<Accessories.PlutoniumAmulet>(), 40);
+            case NPCID.RedDevil:
+                return ItemDropRule.Common(ModContent.ItemType<Scorcher>(), 10);
             case NPCID.HallowBoss:
                 return ItemDropRule.NotScalingWithLuck(ModContent.ItemType<BulletHell>(), 3);
             default:
